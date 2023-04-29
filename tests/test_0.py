cb22530c0ecb9a66dfeaa6b8d9b31f29ce82521b
Daniele Ongari <daniele.ongari@gmail.com>
2023-04-29 10:57:52 +0200

    remove IPython dependency and show output of pytest with -s

--- tests/test_0.py
+++ tests/test_0.py
@@ -1,9 +1,6 @@
 import pandas as pd
 from . import DATA_DIR
 
-from IPython.display import display
-
-
 def test_01():
     """Read a CSV file and create a new column with 3 different methods"""
 
@@ -25,8 +22,12 @@ def test_01():
     # Method 3: load CSV whithin the chaining
     df = (
         pd.read_csv(DATA_DIR / "table_1.csv")
-        .assign(OpenCloseRange=lambda df: df["Open"] - df["Close"])
+        .assign(OpenCloseRange=lambda dfx: dfx["Open"] - dfx["Close"])
     )
     
+    print()
+    print(df.head(3))
+    
     assert "OpenCloseRange" in df.columns
+
     
