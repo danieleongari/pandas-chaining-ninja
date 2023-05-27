3ae039eb562d1ab2bc23cd5e63583349762a4151
Daniele Ongari <daniele.ongari@gmail.com>
2023-05-27 16:40:07 +0200

    constrain pandas version to test dependabot

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
           classifiers=["Programming Language :: Python"],
           version="0.1.0",
           install_requires=[
-              "pandas>=2.0",
+              "pandas==2.0.0",
               "ipython==8.13.0"
           ],
           extras_require={
